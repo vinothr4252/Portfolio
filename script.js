efafc2a50a0730b008adff6c4ec568313c5e4e57
vinothkumar <vinothr4252@gmail.com>
2025-06-20 15:02:37 +0530

    Initial commit: portfolio with contact form

---
+++ script.js
@@ -0,0 +1,8 @@
+//mobile toggle
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click",()=>{
+    navLinks.classList.toggle("active");
+});
